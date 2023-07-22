9e31c9273eb3d16dc0cc7137e34e69a289d74db9
nabs-grg <narbu.grg@gmail.com>
2023-07-22 23:32:36 +0100

    add web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
         </a>
       </header>
       <img src="https://www.w3schools.com/images/w3schools_green.jpg" />
+      <Analytics />
     </div>
   );
 }
